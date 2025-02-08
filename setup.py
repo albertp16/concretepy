b9674e6b79a2e6939880356788cbe29928a1214e
albert pamonag <35522286+albertp16@users.noreply.github.com>
2025-02-08 12:21:12 +0800

    clean codes

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bsdspy',
+    version='0.1.6',
+    description='This APEC internal use only for Concrete Design using ACI and NSCP',
+    author='Albert Pamonag',
+    author_email='albert@apeconsultancy.net',
+    url='https://github.com/albertp16/apec-py',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    company='Albert Pamonag Engineering Consultancy',
+)
